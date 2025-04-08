35e311c3950b1c407900660502963ac31c5e3cd1
Lars Sørensen <larssorensen98@hotmail.com>
2025-04-08 15:18:29 +0200

    Started on authPolicy controller

--- api/v1alpha1/authpolicy_types.go
+++ api/v1alpha1/authpolicy_types.go
@@ -17,20 +17,188 @@ limitations under the License.
 package v1alpha1
 
 import (
+	v1 "istio.io/api/security/v1"
+	"istio.io/api/type/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// AuthPolicySpec defines the desired state of AuthPolicy.
+type AuthPolicySpec struct {
+	// JWTRules specifies how incoming requests should be allowed or denied based on the presence and validation of accompanying JWTs.
+	JWTRules RequestAuthList `json:"jwtRules"`
 
-// AuthPolicySpec defines the desired state of AuthPolicy
-type AuthPolicySpec struct{}
+	// Selector specifies which workload the defined auth policy should be applied to.
+	Selector v1beta1.WorkloadSelector `json:"selector"`
+}
+
+type RequestAuthList []RequestAuth
+
+// RequestAuth specifies how incoming JWTs should be validated.
+//
+// +kubebuilder:object:generate=true
+type RequestAuth struct {
+	// Whether to enable JWT validation.
+	// If enabled, incoming JWTs will be validated against the issuer specified in the app registration and the generated audience.
+	Enabled bool `json:"enabled"`
+
+	// The name of the Kubernetes Secret containing OAuth2 credentials.
+	//
+	// +kubebuilder:validation:Optional
+	SecretName string `json:"secretName"`
+
+	// If set to `true`, the original token will be kept for the upstream request. Defaults to `true`.
+	// +kubebuilder:default=true
+	ForwardJwt bool `json:"forwardJwt,omitempty"`
+
+	// FromCookies denotes the cookies from which the auth policy will look for a JWT.
+	//
+	// +kubebuilder:validation:Optional
+	FromCookies []string `json:"fromCookies,omitempty"`
+
+	// OutputClaimsToHeaders specifies a list of operations to copy the claim to HTTP headers on a successfully verified token.
+	// The header specified in each operation in the list must be unique. Nested claims of type string/int/bool is supported as well.
+	// ```
+	//
+	//	outputClaimToHeaders:
+	//	- header: x-my-company-jwt-group
+	//	  claim: my-group
+	//	- header: x-test-environment-flag
+	//	  claim: test-flag
+	//	- header: x-jwt-claim-group
+	//	  claim: nested.key.group
+	//
+	// ```
+	// +kubebuilder:validation:Optional
+	OutputClaimToHeaders *[]ClaimToHeader `json:"outputClaimToHeaders,omitempty"`
+
+	// AcceptedResources is used as a validation field following [RFC8707](https://datatracker.ietf.org/doc/html/rfc8707).
+	// It defines accepted audience resource indicators in the JWT token.
+	//
+	// Each resource indicator must be a valid URI and the indicator must be present as the `aud` claim in the JWT token.
+	//
+	// +kubebuilder:validation:Optional
+	// +listType=set
+	// +kubebuilder:validation:Items.Pattern=`^(https?):\/\/[^\s\/$.?#].[^\s]*$`
+	AcceptedResources []string `json:"acceptedResources,omitempty"`
+
+	// AuthRules defines rules for allowing HTTP requests based on conditions
+	// that must be met based on JWT claims.
+	//
+	// API endpoints not covered by AuthRules IgnoreAuth requires an authenticated JWT by default.
+	//
+	// +kubebuilder:validation:Optional
+	AuthRules *[]RequestAuthRule `json:"authRules,omitempty"`
+
+	// IgnoreAuth defines request matchers for HTTP requests that do not require JWT authentication.
+	//
+	// API endpoints not covered by AuthRules or IgnoreAuth requires an authenticated JWT by default.
+	//
+	// +kubebuilder:validation:Optional
+	IgnoreAuth *[]RequestMatcher `json:"ignoreAuth,omitempty"`
+}
+
+// ClaimToHeader specifies a list of operations to copy the claim to HTTP headers on a successfully verified token.
+// The header specified in each operation in the list must be unique. Nested claims of type string/int/bool is supported as well.
+//
+// +kubebuilder:object:generate=true
+type ClaimToHeader struct {
+	// Header specifies the name of the HTTP header to which the claim value will be copied.
+	//
+	// +kubebuilder:validation:Pattern="^[a-zA-Z0-9-]+$"
+	// +kubebuilder:validation:MaxLength=64
+	Header string `json:"header"`
+
+	// Claim specifies the name of the claim in the JWT token that will be copied to the header.
+	//
+	// +kubebuilder:validation:Pattern="^[a-zA-Z0-9-._]+$"
+	// +kubebuilder:validation:MaxLength=128
+	Claim string `json:"claim"`
+}
+
+type RequestAuthRules []RequestAuthRule
+
+// RequestAuthRule defines a rule for controlling access to HTTP requests using JWT authentication.
+//
+// +kubebuilder:object:generate=true
+type RequestAuthRule struct {
+	RequestMatcher `json:",inline"`
+
+	// When defines additional conditions based on JWT claims that must be met.
+	//
+	// The request is permitted if at least one of the specified conditions is satisfied.
+	When []Condition `json:"when"`
+}
+
+type RequestMatchers []RequestMatcher
+
+// RequestMatcher defines paths and methods to match incoming HTTP requests.
+//
+// +kubebuilder:object:generate=true
+type RequestMatcher struct {
+	// Paths specifies a set of URI paths that this rule applies to.
+	// Each path must be a valid URI path, starting with '/' and not ending with '/'.
+	// The wildcard '*' is allowed only at the end of the path.
+	//
+	// +listType=set
+	// +kubebuilder:validation:Items.Pattern=`^/[a-zA-Z0-9\-._~!$&'()+,;=:@%/]*(\*)?$`
+	Paths []string `json:"paths"`
+
+	// Methods specifies HTTP methods that applies for the defined paths.
+	// If omitted, all methods are permitted.
+	//
+	// Allowed methods:
+	// - GET
+	// - POST
+	// - PUT
+	// - PATCH
+	// - DELETE
+	// - HEAD
+	// - OPTIONS
+	// - TRACE
+	// - CONNECT
+	//
+	// +listType=set
+	// +kubebuilder:validation:Items:Enum=GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS,TRACE,CONNECT
+	Methods []string `json:"methods,omitempty"`
+}
+
+// Condition represents a rule that evaluates JWT claims to determine access control.
+//
+// This type allows defining conditions that check whether a specific claim in
+// the JWT token contains one of the expected values.
+//
+// If multiple conditions are specified, all must be met (AND logic) for the request to be allowed.
+//
+// +kubebuilder:object:generate=true
+type Condition struct {
+	// Claim specifies the name of the JWT claim to check.
+	//
+	Claim string `json:"claim"`
+
+	// Values specifies a list of allowed values for the claim.
+	// If the claim in the JWT contains any of these values (OR logic), the condition is met.
+	//
+	// +listType=set
+	Values []string `json:"values"`
+}
 
 // AuthPolicyStatus defines the observed state of AuthPolicy
 type AuthPolicyStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	ObservedGeneration int64              `json:"observedGeneration,omitempty"`
+	Conditions         []metav1.Condition `json:"conditions,omitempty"`
+	Ready              bool               `json:"ready"`
+	Phase              Phase              `json:"phase,omitempty"`
+	Message            string             `json:"message,omitempty"`
 }
+
+type Phase string
+
+const (
+	PhasePending Phase = "Pending"
+	PhaseReady   Phase = "Ready"
+	PhaseFailed  Phase = "Failed"
+	PhaseUnknown Phase = "Unknown"
+)
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
@@ -55,4 +223,23 @@ type AuthPolicyList struct {
 
 func init() {
 	SchemeBuilder.Register(&AuthPolicy{}, &AuthPolicyList{})
+}
+
+func (ap *AuthPolicy) InitializeConditions() {
+	if ap.Status.Conditions == nil {
+		ap.Status.Conditions = []metav1.Condition{}
+	}
+}
+
+func (r *RequestAuthList) ToIstioRequestAuthenticationJWTRules() []*v1.JWTRule {
+	var jwtRules []*v1.JWTRule
+	for _, rule := range *r {
+		jwtRule := &v1.JWTRule{
+			Issuer:    "https://example-issuer.com",
+			Audiences: []string{"example-audience"},
+			JwksUri:   "https://example-issuer.com/jwks",
+		}
+		jwtRules = append(jwtRules, jwtRule)
+	}
+	return jwtRules
 }
--- internal/controller/authpolicy_controller.go
+++ internal/controller/authpolicy_controller.go
@@ -18,11 +18,20 @@ package controller
 
 import (
 	"context"
+	"github.com/gogo/protobuf/proto"
+	v3 "istio.io/api/security/v1"
+	"istio.io/api/security/v1beta1"
+	v1 "istio.io/client-go/pkg/apis/security/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/apimachinery/pkg/api/meta"
+	v2 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/errors"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	ztoperatorv1alpha1 "github.com/kartverket/ztoperator/api/v1alpha1"
 )
@@ -33,30 +42,321 @@ type AuthPolicyReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=ztoperator.kartverket.no,resources=authpolicies,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=ztoperator.kartverket.no,resources=authpolicies/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=ztoperator.kartverket.no,resources=authpolicies/finalizers,verbs=update
-
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the AuthPolicy object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.18.4/pkg/reconcile
-func (r *AuthPolicyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
-
-	// TODO(user): your logic here
-
-	return ctrl.Result{}, nil
-}
-
 // SetupWithManager sets up the controller with the Manager.
 func (r *AuthPolicyReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&ztoperatorv1alpha1.AuthPolicy{}).
+		Owns(&v1.RequestAuthentication{}).
+		Owns(&v1.AuthorizationPolicy{}).
 		Complete(r)
+}
+
+type authPolicyReconcileFunc func(context.Context, *scope) (ctrl.Result, error)
+
+type scope struct {
+	authPolicy  *ztoperatorv1alpha1.AuthPolicy
+	descendants authPolicyDescendants
+}
+
+type authPolicyDescendants struct {
+	requestAuthentication          *v1.RequestAuthentication
+	ignoreAuthAuthorizationPolicy  *v1.AuthorizationPolicy
+	requireAuthAuthorizationPolicy *v1.AuthorizationPolicy
+}
+
+// +kubebuilder:rbac:groups=ztoperator.kartverket.no,resources=authpolicies,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=ztoperator.kartverket.no,resources=authpolicies/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=ztoperator.kartverket.no,resources=authpolicies/finalizers,verbs=update
+// +kubebuilder:rbac:groups=security.istio.io,resources=authorizationpolicies;requestauthentications,verbs=get;list;watch;create;update;patch;delete
+
+func (r *AuthPolicyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := ctrl.LoggerFrom(ctx)
+	authPolicy := new(ztoperatorv1alpha1.AuthPolicy)
+
+	if err := r.Client.Get(ctx, req.NamespacedName, authPolicy); err != nil {
+		if apierrors.IsNotFound(err) {
+			log.Error(err, "AuthPolicy with name %s not found", req.NamespacedName.String())
+			return reconcile.Result{}, nil
+		}
+		log.Error(err, "Failed to get AuthPolicy with name %s", req.NamespacedName.String())
+		return reconcile.Result{}, err
+	}
+
+	// TODO: Finalize object if it's being deleted
+	// TODO: Add finalizers + r.Client.Update() if missing
+
+	originalAuthPolicy := authPolicy.DeepCopy()
+	authPolicy.InitializeConditions()
+
+	s := &scope{authPolicy: authPolicy}
+
+	defer func() {
+		r.updateStatus(ctx, s)
+	}()
+
+	// TODO: 5. if (originalAuthPolicy.Status != authPolicy.Status): client.Status.Patch(obj)
+	if !equality.Semantic.DeepEqual(originalAuthPolicy.Status, authPolicy.Status) {
+		if err := r.Status().Patch(ctx, authPolicy, client.MergeFrom(originalAuthPolicy)); err != nil {
+			log.Error(err, "Failed to patch AuthPolicy status with name %s", req.NamespacedName.String())
+			return ctrl.Result{}, err
+		}
+	}
+
+	reconcileFuncs := []authPolicyReconcileFunc{
+		r.reconcileRequestAuthentication,
+		r.reconcileIgnoreAuthAuthorizationPolicy,
+		r.reconcileRequireAuthAuthorizationPolicy,
+	}
+
+	if !authPolicy.DeletionTimestamp.IsZero() {
+		reconcileFuncs = append(reconcileFuncs, r.reconcileDelete)
+	}
+
+	return doReconcile(ctx, reconcileFuncs, s)
+}
+
+func doReconcile(ctx context.Context, reconcileFuncs []authPolicyReconcileFunc, s *scope) (ctrl.Result, error) {
+	result := ctrl.Result{}
+	var errs []error
+	for _, reconcileFunc := range reconcileFuncs {
+		reconcileResult, err := reconcileFunc(ctx, s)
+		if err != nil {
+			errs = append(errs, err)
+		}
+		if len(errs) > 0 {
+			continue
+		}
+		result = lowestNonZeroResult(result, reconcileResult)
+	}
+
+	if len(errs) > 0 {
+		return ctrl.Result{}, errors.NewAggregate(errs)
+	}
+	return result, nil
+}
+
+func lowestNonZeroResult(i, j ctrl.Result) ctrl.Result {
+	switch {
+	case i.IsZero():
+		return j
+	case j.IsZero():
+		return i
+	case i.Requeue:
+		return i
+	case j.Requeue:
+		return j
+	case i.RequeueAfter < j.RequeueAfter:
+		return i
+	default:
+		return j
+	}
+}
+
+func (r *AuthPolicyReconciler) updateStatus(ctx context.Context, s *scope) {
+	ap := s.authPolicy
+	log := ctrl.LoggerFrom(ctx)
+
+	ap.Status.ObservedGeneration = ap.GetGeneration()
+
+	phase := ztoperatorv1alpha1.PhaseReady
+	condition := v2.Condition{
+		Type:               string(ztoperatorv1alpha1.PhaseReady),
+		Status:             v2.ConditionTrue,
+		LastTransitionTime: v2.Now(),
+		Reason:             "ReconciliationSucceeded",
+		Message:            "Descendants created or updated successfully",
+	}
+
+	if s.descendants.requestAuthentication.Name == "" || s.descendants.ignoreAuthAuthorizationPolicy.Name == "" || s.descendants.requireAuthAuthorizationPolicy.Name == "" {
+		phase = ztoperatorv1alpha1.PhasePending
+		condition = v2.Condition{
+			Type:               string(ztoperatorv1alpha1.PhasePending),
+			Status:             v2.ConditionFalse,
+			LastTransitionTime: v2.Now(),
+			Reason:             "MissingDescendants",
+			Message:            "Descendants resources are not yet created",
+		}
+	}
+
+	ap.Status.Phase = phase
+	meta.SetStatusCondition(&ap.Status.Conditions, condition)
+
+	if err := r.Status().Patch(ctx, ap, client.MergeFrom(ap)); err != nil {
+		log.Error(err, "Failed to patch AuthPolicy status")
+	}
+}
+
+func (r *AuthPolicyReconciler) reconcileRequestAuthentication(ctx context.Context, scope *scope) (ctrl.Result, error) {
+	authPolicy := scope.authPolicy
+
+	desired := &v1.RequestAuthentication{
+		ObjectMeta: v2.ObjectMeta{
+			Name:      authPolicy.Name,
+			Namespace: authPolicy.Namespace,
+			Labels:    map[string]string{"type": "ztoperator.kartverket.no"},
+		},
+		Spec: v1beta1.RequestAuthentication{
+			Selector: &authPolicy.Spec.Selector,
+			JwtRules: authPolicy.Spec.JWTRules.ToIstioRequestAuthenticationJWTRules(),
+		},
+	}
+
+	current := new(v1.RequestAuthentication)
+	err := r.Client.Get(ctx, client.ObjectKeyFromObject(desired), current)
+	if apierrors.IsNotFound(err) {
+		if err := ctrl.SetControllerReference(authPolicy, desired, r.Scheme); err != nil {
+			return ctrl.Result{}, err
+		}
+		if err := r.Client.Create(ctx, desired); err != nil {
+			return ctrl.Result{}, err
+		}
+		scope.descendants.requestAuthentication = desired
+		return ctrl.Result{}, nil
+	}
+	if !proto.Equal(&current.Spec, &desired.Spec) {
+		current.Spec.Selector = desired.Spec.Selector
+		current.Spec.JwtRules = desired.Spec.JwtRules
+		if err := r.Client.Update(ctx, current); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	scope.descendants.requestAuthentication = current
+	return ctrl.Result{}, nil
+}
+
+func (r *AuthPolicyReconciler) reconcileIgnoreAuthAuthorizationPolicy(ctx context.Context, scope *scope) (ctrl.Result, error) {
+	authPolicy := scope.authPolicy
+
+	desired := &v1.AuthorizationPolicy{
+		ObjectMeta: v2.ObjectMeta{
+			Name:      authPolicy.Name,
+			Namespace: authPolicy.Namespace,
+			Labels:    map[string]string{"type": "ztoperator.kartverket.no"},
+		},
+		Spec: v1beta1.AuthorizationPolicy{
+			Selector: &authPolicy.Spec.Selector,
+			Rules: []*v3.Rule{
+				{
+					To: []*v1beta1.Rule_To{
+						{
+							Operation: &v1beta1.Operation{
+								Paths: []string{"*"},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	current := new(v1.AuthorizationPolicy)
+	err := r.Client.Get(ctx, client.ObjectKeyFromObject(desired), current)
+	if apierrors.IsNotFound(err) {
+		if err := ctrl.SetControllerReference(authPolicy, desired, r.Scheme); err != nil {
+			return ctrl.Result{}, err
+		}
+		if err := r.Client.Create(ctx, desired); err != nil {
+			return ctrl.Result{}, err
+		}
+		scope.descendants.ignoreAuthAuthorizationPolicy = desired
+		return ctrl.Result{}, nil
+	}
+	if !proto.Equal(&current.Spec, &desired.Spec) {
+		current.Spec.Selector = desired.Spec.Selector
+		current.Spec.Rules = desired.Spec.Rules
+		if err := r.Client.Update(ctx, current); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	scope.descendants.ignoreAuthAuthorizationPolicy = current
+	return ctrl.Result{}, nil
+}
+
+func (r *AuthPolicyReconciler) reconcileRequireAuthAuthorizationPolicy(ctx context.Context, scope *scope) (ctrl.Result, error) {
+	authPolicy := scope.authPolicy
+
+	desired := &v1.AuthorizationPolicy{
+		ObjectMeta: v2.ObjectMeta{
+			Name:      authPolicy.Name,
+			Namespace: authPolicy.Namespace,
+			Labels:    map[string]string{"type": "ztoperator.kartverket.no"},
+		},
+		Spec: v1beta1.AuthorizationPolicy{
+			Selector: &authPolicy.Spec.Selector,
+			Rules: []*v3.Rule{
+				{
+					To: []*v1beta1.Rule_To{
+						{
+							Operation: &v1beta1.Operation{
+								Paths: []string{"*"},
+							},
+						},
+					},
+					When: []*v1beta1.Condition{
+						{
+							Key:    "request.auth.claims[iss]",
+							Values: []string{"example-issuer"},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	current := new(v1.AuthorizationPolicy)
+	err := r.Client.Get(ctx, client.ObjectKeyFromObject(desired), current)
+	if apierrors.IsNotFound(err) {
+		if err := ctrl.SetControllerReference(authPolicy, desired, r.Scheme); err != nil {
+			return ctrl.Result{}, err
+		}
+		if err := r.Client.Create(ctx, desired); err != nil {
+			return ctrl.Result{}, err
+		}
+		scope.descendants.requireAuthAuthorizationPolicy = desired
+		return ctrl.Result{}, nil
+	}
+	if !proto.Equal(&current.Spec, &desired.Spec) {
+		current.Spec.Selector = desired.Spec.Selector
+		current.Spec.Rules = desired.Spec.Rules
+		if err := r.Client.Update(ctx, current); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	scope.descendants.requireAuthAuthorizationPolicy = current
+	return ctrl.Result{}, nil
+}
+
+func (r *AuthPolicyReconciler) reconcileDelete(ctx context.Context, scope *scope) (ctrl.Result, error) {
+	authPolicy := scope.authPolicy
+
+	resources := []client.Object{
+		&v1.RequestAuthentication{
+			ObjectMeta: v2.ObjectMeta{
+				Name:      authPolicy.Name,
+				Namespace: authPolicy.Namespace,
+			},
+		},
+		&v1.AuthorizationPolicy{
+			ObjectMeta: v2.ObjectMeta{
+				Name:      authPolicy.Name,
+				Namespace: authPolicy.Namespace,
+			},
+		},
+	}
+
+	var errs []error
+	for _, resource := range resources {
+		if err := r.Client.Delete(ctx, resource); err != nil && !apierrors.IsNotFound(err) {
+			errs = append(errs, err)
+		}
+	}
+
+	// TODO: If we have finalizers, remove them (example: "ztoperator.kartverket.no/finalizer")
+	if len(errs) > 0 {
+		return ctrl.Result{}, errors.NewAggregate(errs)
+	}
+	return ctrl.Result{}, nil
 }
