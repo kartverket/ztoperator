06c9f4a9a90be9960fb0bc86e398b9f17522eac8
Herman Wika Horn <herman.horn@bekk.no>
2026-01-29 14:34:20 +0100

    Refactor require rules to favor early return + enhance readability
    This is perhaps the most complicated of the three policy files.
    While creating deny- and allow-rules for deny and ignore rules
    respectively is rather easy, the require rules depend on both
    defined AuthRules and IgnoreAuthRules. The refactored code tries
    to highlight the types of policies created. These are as follows:
    
    - If no auth rules or ignore auth rules are specified, we create
      an api-wide validation of audience and issuer
    
    - Otherwise, we create three types of allow rules:
      - 1) Rules for the specified paths and methods, with
           aud-/iss-conditions and additional conditions in authRules
      - 2) Rules for the specified paths for all methods _not_
           specified in the rule, with aud-/iss-conditions
      - 3) Rules for all _other_ paths than those mentioned in auth-
           and ignoreAuth-rules, with aud-/iss-conditions
    
    Comments are added to highlight the use of NotMethods, NotValues
    and NotPaths, because at a glance it can be difficult to
    distinguish these from regular Methods-/Values-/Paths operations.

--- pkg/resourcegenerators/authorizationpolicy/require/authorization_policy.go
+++ pkg/resourcegenerators/authorizationpolicy/require/authorization_policy.go
@@ -7,7 +7,6 @@ import (
 	"github.com/kartverket/ztoperator/internal/state"
 	"github.com/kartverket/ztoperator/pkg/resourcegenerators/authorizationpolicy"
 	"istio.io/api/security/v1beta1"
-	v1beta2 "istio.io/api/type/v1beta1"
 	istioclientsecurityv1 "istio.io/client-go/pkg/apis/security/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -18,89 +17,129 @@ func GetDesired(scope *state.Scope, objectMeta v1.ObjectMeta) *istioclientsecuri
 		return nil
 	}
 
-	var authorizationPolicyRules []*v1beta1.Rule
-
 	audienceAndIssuerConditions := authorizationpolicy.GetAudienceAndIssuerConditionsForAllowPolicy(
 		authorizationpolicy.ConstructAcceptedResources(*scope),
 		scope.IdentityProviderUris.IssuerURI,
 	)
 
-	if (scope.AuthPolicy.Spec.AuthRules == nil || len(*scope.AuthPolicy.Spec.AuthRules) == 0) &&
-		(scope.AuthPolicy.Spec.IgnoreAuthRules == nil || len(*scope.AuthPolicy.Spec.IgnoreAuthRules) == 0) {
-		authorizationPolicyRules = append(authorizationPolicyRules, &v1beta1.Rule{
-			To: []*v1beta1.Rule_To{
-				{
-					Operation: &v1beta1.Operation{
-						Paths: []string{"*"},
+	hasAuthRules := scope.AuthPolicy.Spec.AuthRules != nil && len(*scope.AuthPolicy.Spec.AuthRules) > 0
+	hasIgnoreAuthRules := scope.AuthPolicy.Spec.IgnoreAuthRules != nil &&
+		len(*scope.AuthPolicy.Spec.IgnoreAuthRules) > 0
+
+	if !hasAuthRules && !hasIgnoreAuthRules {
+		// If there are no specific auth rules or ignore rules, we create an allow-rule
+		// matching all paths and all methods, with validation of audience and issuer.
+		allPathsRule := []*v1beta1.Rule{
+			{
+				To: []*v1beta1.Rule_To{
+					{
+						Operation: &v1beta1.Operation{
+							Paths: []string{"*"},
+						},
 					},
 				},
+				When: audienceAndIssuerConditions,
 			},
-			When: baseConditions,
-		})
-	} else {
-		var toList []*v1beta1.Rule_To
-		var mentionedPaths []string
-		for _, matcher := range append(
-			scope.AuthPolicy.GetRequireAuthRequestMatchers(),
-			scope.AuthPolicy.GetIgnoreAuthRequestMatchers()...,
-		) {
-			mentionedPaths = append(mentionedPaths, matcher.Paths...)
-			methods := matcher.Methods
-			if len(matcher.Methods) == 0 {
-				methods = v1alpha1.GetAcceptedHTTPMethods()
-			}
-			toList = append(toList, &v1beta1.Rule_To{
-				Operation: &v1beta1.Operation{
-					Paths:      matcher.Paths,
-					NotMethods: methods,
-				},
-			})
 		}
 		return authorizationpolicy.AllowAuthorizationPolicy(scope, objectMeta, allPathsRule)
+	}
 
-		toList = append(toList, &v1beta1.Rule_To{
-			Operation: &v1beta1.Operation{
-				Paths:    []string{"*"},
-				NotPaths: mentionedPaths,
-			},
-		})
+	specifiedPathsRules := constructSpecifiedPathsAllowRules(scope, audienceAndIssuerConditions)
 
-		authorizationPolicyRules = append(authorizationPolicyRules, &v1beta1.Rule{
-			To:   toList,
-			When: baseConditions,
-		})
-		if scope.AuthPolicy.Spec.AuthRules != nil {
-			for _, authRule := range *scope.AuthPolicy.Spec.AuthRules {
-				authPolicyConditionsAsIstioConditions := baseConditions
-				if authRule.When != nil {
-					for _, condition := range *authRule.When {
-						authPolicyConditionsAsIstioConditions = append(authPolicyConditionsAsIstioConditions, &v1beta1.Condition{
+	unspecifiedPathsRule := constructUnspecifiedPathsAllowRule(scope, audienceAndIssuerConditions)
+
+	allAllowRules := append([]*v1beta1.Rule{unspecifiedPathsRule}, specifiedPathsRules...)
 	return authorizationpolicy.AllowAuthorizationPolicy(
 		scope,
 		objectMeta,
 		allAllowRules,
 	)
+}
+
+/*
+Each auth rule should result in an allow rule for the specified paths, methods and conditions.
+Additionally, the audience and issuer conditions are always included.
+*/
+func constructSpecifiedPathsAllowRules(
+	scope *state.Scope,
+	audienceAndIssuerConditions []*v1beta1.Condition,
+) []*v1beta1.Rule {
+	var specifiedPathsAllowRules []*v1beta1.Rule
+	if scope.AuthPolicy.Spec.AuthRules != nil {
+		for _, authRule := range *scope.AuthPolicy.Spec.AuthRules {
+			authPolicyConditionsAsIstioConditions := audienceAndIssuerConditions
+			if authRule.When != nil {
+				for _, condition := range *authRule.When {
+					authPolicyConditionsAsIstioConditions = append(
+						authPolicyConditionsAsIstioConditions,
+						&v1beta1.Condition{
 							Key:    fmt.Sprintf("request.auth.claims[%s]", condition.Claim),
 							Values: condition.Values,
-						})
-					}
+						},
+					)
 				}
-				authorizationPolicyRules = append(authorizationPolicyRules, &v1beta1.Rule{
-					To: []*v1beta1.Rule_To{
-						{
-							Operation: &v1beta1.Operation{
-								Paths:   authRule.Paths,
-								Methods: authRule.Methods,
-							},
+			}
+			specifiedPathsAllowRules = append(specifiedPathsAllowRules, &v1beta1.Rule{
+				To: []*v1beta1.Rule_To{
+					{
+						Operation: &v1beta1.Operation{
+							Paths:   authRule.Paths,
+							Methods: authRule.Methods,
 						},
 					},
-					When: authPolicyConditionsAsIstioConditions,
-				})
-			}
+				},
+				When: authPolicyConditionsAsIstioConditions,
+			})
 		}
 	}
+	return specifiedPathsAllowRules
+}
+
+/*
+All paths and methods not explicitly specified in any auth rule or ignore auth rule
+should result in an allow rule with only audience and issuer conditions.
+*/
+func constructUnspecifiedPathsAllowRule(
+	scope *state.Scope,
+	audienceAndIssuerConditions []*v1beta1.Condition,
+) *v1beta1.Rule {
+	var unspecifiedPathsRuleList []*v1beta1.Rule_To
+
+	allRequestMatchers := append(
+		scope.AuthPolicy.GetRequireAuthRequestMatchers(),
+		scope.AuthPolicy.GetIgnoreAuthRequestMatchers()...,
+	)
+
+	// For all request matchers, create to-rules for all methods not defined in the matcher
+	for _, matcher := range allRequestMatchers {
+		methods := matcher.Methods
+		if len(matcher.Methods) == 0 {
+			methods = v1alpha1.GetAcceptedHTTPMethods()
+		}
+		unspecifiedPathsRuleList = append(unspecifiedPathsRuleList, &v1beta1.Rule_To{
+			Operation: &v1beta1.Operation{
+				Paths:      matcher.Paths,
+				NotMethods: methods, // NB: NotMethods used to create to-rules for all methods not defined in a matcher
 			},
-			Rules: authorizationPolicyRules,
-		},
+		})
 	}
+
+	// For all request matchers, create to-rules for all paths not defined in a matcher
+	var mentionedPaths []string
+	for _, matcher := range allRequestMatchers {
+		mentionedPaths = append(mentionedPaths, matcher.Paths...)
+	}
+	unspecifiedPathsRuleList = append(unspecifiedPathsRuleList, &v1beta1.Rule_To{
+		Operation: &v1beta1.Operation{
+			Paths:    []string{"*"},
+			NotPaths: mentionedPaths, // NB! NotPaths used to create to-rule for all paths not defined in any matcher
+		},
+	})
+
+	// Create allow rule for all unspecified paths and methods, with audience and issuer conditions
+	unspecifiedPathsAllowRule := &v1beta1.Rule{
+		To:   unspecifiedPathsRuleList,
+		When: audienceAndIssuerConditions,
+	}
+	return unspecifiedPathsAllowRule
 }
